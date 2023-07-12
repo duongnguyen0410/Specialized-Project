71494bb127a25c4e7b781fa59eb19245fd7b406e
Duong Nguyen <20521220@gm.uit.edu.vn>
2023-07-12 21:41:40 +0700

    delete customer_migrate

--- private_bcn/migrations/2_customers_register_migrage.js
+++
@@ -1,7 +0,0 @@
-// Help Truffle find `TruffleTutorial.sol` in the `/contracts` directory
-const CustomersRegistry = artifacts.require("CustomersRegistry");
-
-module.exports = function(deployer) {
-  // Command Truffle to deploy the Smart Contract
-  deployer.deploy(CustomersRegistry);
-};
